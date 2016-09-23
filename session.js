92b0979ae44846c2035591b78e277f48eb492a1b
Michael Pontus <m.pontus@gmail.com>
2016-09-23 13:26:53 +0300

    Persistent sessions

--- session.js
+++ session.js
@@ -1,9 +1,14 @@
+var path = require('path');
 var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 var uuid = require('node-uuid');
 
 require('dotenv').config();
 
 var sessionInstance = session({
+  store: new FileStore({
+    path: path.join(__dirname, 'data', 'sessions'),
+  }),
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
