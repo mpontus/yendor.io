80c696f87a844294c1b7794146819db4e7a6e0ef
Michael Pontus <m.pontus@gmail.com>
2016-09-23 13:31:11 +0300

    Simplify app.js

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
-var express = require('express');
+var fs = require('fs');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-// var term = require('term.js');
-// var babelifyExpress = require('babelify-express');
+var express = require('express');
 var browserify = require('browserify-middleware');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
 var session = require('./session');
+var games = require('./games');
 
 var app = express();
 
@@ -18,63 +12,53 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-// app.use(term.middleware());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(session);
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session);
+app.get('/', function(req, res, next) {
+  res.render('index');
+});
 
-app.use('/', routes);
-app.use('/users', users);
+app.get('/play/:game', function(req, res, next) {
+  var game = req.params.game;
+  if (!games[game]) {
+    var err = new Error("Not Found");
+    err.status = 404;
+    return next(err);
+  }
+  var options = games[game];
+  var readmePath = path.join(__dirname, 'docs', game, 'readme.txt');
+  fs.readFile(readmePath, function(err, readmeContents) {
+    res.render('play', {
+      game: game,
+      title: options.title,
+      term: options.term,
+      readme: readmeContents,
+    });
+  });
+});
 
 app.get('/js/client.js', browserify('./client/index.js', {
   debug: true,
   transform: [ "brfs" ]
 }));
 
-// app.use(browserify('/js/client.js', {
-//   entry: __dirname + '/client/index.js',
-//   watch: __dirname + '/client',
-//   mount: '/js/client.js',
-//   // minify: true,
-//   write_file: __dirname + '/public/js/client.js',
-// }));
-
 // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
-// error handlers
-
-// // development error handler
-// // will print stacktrace
-// if (app.get('env') === 'development') {
-//   app.use(function(err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//       message: err.message,
-//       error: err
-//     });
-//   });
-// }
-
-// // production error handler
-// // no stacktraces leaked to user
-// app.use(function(err, req, res, next) {
-//   res.status(err.status || 500);
-//   res.render('error', {
-//     message: err.message,
-//     error: {}
-//   });
-// });
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 
 
 module.exports = app;
--- games/index.js
+++ games/index.js
@@ -1,17 +1,32 @@
 module.exports = {
   'rogue': {
+    'title': "Rogue 5.4.4",
     'dockerImage': 'yendor/rogue',
     'gameDataDir': '/root',
     'attachAfterStart': false,
+    'term': {
+      'cols': 80,
+      'rows': 24,
+    },
   },
   'nethack': {
+    'title': "Nethack 3.6.0",
     'dockerImage': 'yendor/nethack',
     'gameDataDir': '/root/nh/install/games/lib/nethackdir/save/',
     'attachAfterStart': false,
+    'term': {
+      'cols': 80,
+      'rows': 24,
+    },
   },
   'brogue': {
+    'title': "Brogue 1.7.4",
     'dockerImage': 'yendor/brogue',
     'gameDataDir': '/root',
     'attachAfterStart': true,
+    'term': {
+      'cols': 100,
+      'rows': 34,
+    },
   },
 };
